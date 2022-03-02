3488aaeaeb9bb329c1edcfc8180f8672e040e1ca
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-02 16:44:43 +0100

    da sistemare numero elementi

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
-let btnPlay = document.getElementById('btnPlay');
-let gridSpace = document.body.querySelector('#gridSpace');
+const btnPlay = document.getElementById('btnPlay');
+const gridSpace = document.body.querySelector('#gridSpace');
+
+window.addEventListener('resize', squareBoxes)
 
 btnPlay.addEventListener('click', createGrid);
 
@@ -10,5 +12,20 @@ function createGrid(){
         block.classList.add('box');
         block.innerHTML = `<span>${i}</span>`
         gridSpace.append(block);
+    }
+
+    squareBoxes();
+}
+
+function squareBoxes(){
+    const boxes = document.querySelectorAll('.box');
+    const elementBox = document.querySelector('.box');
+
+    const width = elementBox.offsetWidth;
+
+    console.log(width)
+
+    for (i=0; i<boxes.length; i++){
+        boxes[i].setAttribute('style', `height: ${width}px`);
     }
 }
