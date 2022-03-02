0e4a4f824fb3a2a307bd2591061ed8be0dd3f9c1
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-03 00:44:45 +0100

    funzionante

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,41 @@
 const btnPlay = document.getElementById('btnPlay');
-const gridSpace = document.body.querySelector('#gridSpace');
 
-let blockNumber = 100;
+const gridSpaceContainer = document.body.querySelector('#gridSpaceContainer');
+const difficultySelector = document.body.querySelector('#level');
+
+let areThereElements = false;
+let blockNumber = 0;
 
 window.addEventListener('resize', squareBoxes)
+window.addEventListener('load', createGrid)
 
 btnPlay.addEventListener('click', createGrid);
 
 function createGrid(){
+
+    if(areThereElements){
+        gridSpaceContainer.removeChild(document.body.querySelector('#gridSpace'));
+    }
+    
+    switch (difficultySelector.value){
+        case '0':
+            blockNumber =100
+            break;
+        case '1':
+            blockNumber = 81
+            break;
+        case '2':
+            blockNumber = 49
+            break;
+    }
+    
+    let gridSpace = document.createElement('div');
+
+    gridSpace.setAttribute('id', 'gridSpace');
+    gridSpace.setAttribute('class', 'col d-flex flex-wrap justify-content-center align-content-center')
+    gridSpaceContainer.append(gridSpace);
+
+    areThereElements = true;
     
     for(let i = 1; i <= blockNumber; i++){
         let block = document.createElement('div');
@@ -22,6 +50,7 @@ function createGrid(){
     }
 
     squareBoxes();
+    
 }
 
 function squareBoxes(){
