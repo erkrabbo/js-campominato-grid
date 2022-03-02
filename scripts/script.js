764ecc262636c1e93868ffbdea8ac0684f256c90
Luca Andaloro <luca.andaloro4@gmail.com>
2022-03-02 17:53:34 +0100

    selezione difficolta e rivedere altezze

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,23 @@
 const btnPlay = document.getElementById('btnPlay');
 const gridSpace = document.body.querySelector('#gridSpace');
 
+let blockNumber = 100;
+
 window.addEventListener('resize', squareBoxes)
 
 btnPlay.addEventListener('click', createGrid);
 
 function createGrid(){
-    let blockNumber = 100;
+    
     for(let i = 1; i <= blockNumber; i++){
         let block = document.createElement('div');
         block.classList.add('box');
         block.innerHTML = `<span>${i}</span>`
+        if (i % 10 == 1){
+            let separator = document.createElement('div');
+            separator.classList.add('w-100');
+            gridSpace.append(separator);
+        }
         gridSpace.append(block);
     }
 
@@ -29,3 +36,6 @@ function squareBoxes(){
         boxes[i].setAttribute('style', `height: ${width}px`);
     }
 }
+
+
+     
